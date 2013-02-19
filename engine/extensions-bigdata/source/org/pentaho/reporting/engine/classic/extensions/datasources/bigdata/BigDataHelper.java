9dc8c05f3933c25abd05e04babca0779fe3d1f0d
Thomas Morgner <git@rabbit-stew-dio.com>
2013-02-19 17:37:06 +0000

    Added helper function that converts one of the big-data datasources into a plain kettle datasource

--- engine/extensions-bigdata/source/org/pentaho/reporting/engine/classic/extensions/datasources/bigdata/BigDataHelper.java
+++ engine/extensions-bigdata/source/org/pentaho/reporting/engine/classic/extensions/datasources/bigdata/BigDataHelper.java
@@ -1,14 +1,22 @@
 package org.pentaho.reporting.engine.classic.extensions.datasources.bigdata;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.pentaho.di.core.exception.KettlePluginException;
 import org.pentaho.di.datafactory.DynamicDatasource;
+import org.pentaho.reporting.engine.classic.core.MasterReport;
+import org.pentaho.reporting.engine.classic.extensions.datasources.kettle.KettleDataFactory;
+import org.pentaho.reporting.engine.classic.extensions.datasources.kettle.KettleTransFromFileProducer;
+import org.pentaho.reporting.libraries.docbundle.DocumentBundle;
+import org.pentaho.reporting.libraries.docbundle.WriteableDocumentBundle;
 import org.pentaho.reporting.libraries.xmlns.parser.LoggingErrorHandler;
 import org.pentaho.reporting.libraries.xmlns.parser.ParserEntityResolver;
 import org.w3c.dom.Document;
@@ -21,7 +29,7 @@ public class BigDataHelper
   {
   }
 
-  public static Document loadDocumentFromBytes (final byte[] bytes) throws KettlePluginException
+  public static Document loadDocumentFromBytes(final byte[] bytes) throws KettlePluginException
   {
     return loadDocument(new ByteArrayInputStream(bytes));
   }
@@ -35,6 +43,61 @@ public class BigDataHelper
 
     return loadDocument(in);
 
+  }
+
+  public static KettleDataFactory convertEmbedded(final MasterReport report,
+                                                  final BigDataDataFactory dataFactory,
+                                                  final String targetFile,
+                                                  final String query) throws IOException
+  {
+    final DocumentBundle bundle = report.getBundle();
+    if (bundle instanceof WriteableDocumentBundle == false)
+    {
+      throw new IOException();
+    }
+    final BigDataQueryTransformationProducer q = dataFactory.getQuery(query);
+    final WriteableDocumentBundle w = (WriteableDocumentBundle) bundle;
+    final OutputStream entry = w.createEntry(targetFile, "text/xml");
+    try
+    {
+      entry.write(q.getBigDataTransformationRaw());
+    }
+    finally
+    {
+      entry.close();
+    }
+
+    final KettleDataFactory kettleDataFactory = new KettleDataFactory();
+    kettleDataFactory.setQuery(query, new KettleTransFromFileProducer
+        (targetFile, q.getStepName(), q.getDefinedArgumentNames(), q.getDefinedVariableNames()));
+    return kettleDataFactory;
+  }
+
+  public static KettleDataFactory convert(final MasterReport report,
+                                          final BigDataDataFactory dataFactory,
+                                          final File targetFile,
+                                          final String query) throws IOException
+  {
+    final DocumentBundle bundle = report.getBundle();
+    if (bundle instanceof WriteableDocumentBundle == false)
+    {
+      throw new IOException();
+    }
+    final BigDataQueryTransformationProducer q = dataFactory.getQuery(query);
+    final FileOutputStream fout = new FileOutputStream(targetFile);
+    try
+    {
+      fout.write(q.getBigDataTransformationRaw());
+    }
+    finally
+    {
+      fout.close();
+    }
+
+    final KettleDataFactory kettleDataFactory = new KettleDataFactory();
+    kettleDataFactory.setQuery(query, new KettleTransFromFileProducer
+        (targetFile.getName(), q.getStepName(), q.getDefinedArgumentNames(), q.getDefinedVariableNames()));
+    return kettleDataFactory;
   }
 
   public static Document loadDocument(final InputStream in) throws KettlePluginException
